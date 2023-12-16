9790934f26104eba863dfd1254466cc0bd888b47
MikiSimo560 <152563421+MikiSimo560@users.noreply.github.com>
2023-12-16 12:46:34 +0100

    Timer.py

--- Timer.py
+++ Timer.py
@@ -1,16 +1,25 @@
 import time
+from sys import exit
+
 y = int(input("minutes: "))
 x = int(input("seconds: "))
+
 if x > 60:
     y += int(x/60)
     x -= 60 * int(x/60)
+
 if y or x > 0:
     while True:
-        print(y, ":", x)
-        x -= 1
-        if x < 0:
-            x += 60
-            y -= 1
-        time.sleep(1)
+        if y or x > 0:
+            print(y, ":", x)
+            x -= 1
+            if x < 0:
+                x += 60
+                y -= 1
+            time.sleep(1)
+        else:
+            print("done")
+            exit()
 else:
     print("done")
+    exit()
